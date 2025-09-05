c4bd74dc91d9da66b1ee2201f992f7ef90becd4a
mistetrxz <luasanandrezzz@gmail.com>
2025-09-04 21:53:24 -0300

    Implementacao cavalo

--- desafio_xadrez.c
+++ desafio_xadrez.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+
 int main(){
 
     int i;
+    int movimentacao = 1;
 
-    printf("\n-- Bem vindo ao Jogo de Xadrez! --\n");
+    printf("\n-- Bem vindo ao Jogo de Xadrez! --\n\n");
 
     // Movimentação da Torre 
     printf("Movimentacao da Torre:\n");
@@ -29,6 +32,22 @@ int main(){
         printf("Casa %d: Esquerda\n", i);
         i++;
     } while (i <= 8);
+
+    printf("\n");
+
+    //Movimentação do Cavalo
+    printf("Movimentacao do Cavalo:\n");
+    while (movimentacao--)
+    {
+       for ( i = 0; i < 2; i++)
+       {
+        printf("Cima\n");
+       }
+       printf("Direita\n");
+
+       printf("\n");
+    }
+    
 
     return 0;
 }
